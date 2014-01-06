0fb49de97c8e2b224bb96f5a69ee8d98359e3e0d
Cédric M <me.cedric@gmail.com>
2014-01-06 19:13:20 +0100

    Solution to Ruby-0 challenge 6

--- Wagon-0/06. Stupid coaching/stupid_coaching.rb
+++ Wagon-0/06. Stupid coaching/stupid_coaching.rb
@@ -1,5 +1,13 @@
 def stupid_coaching 
   # your code goes here
+  print "Anything to declare?"
+  answer = gets.chomp
+
+  until answer.include? "I'm going to work"
+  	puts "Silly question, get dressed and go to work!" if answer == answer
+  else puts "I don't care son, get dressed and go to work !"
+  end
+  puts "Good! Happy new year!"
 end
 
 # Calling the method
