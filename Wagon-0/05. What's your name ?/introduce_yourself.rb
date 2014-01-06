172f087f4e96e9b4a178332c0b2e8c1c69ba163c
Cédric M <me.cedric@gmail.com>
2014-01-06 18:05:21 +0100

    First solution Ruby-0 challenge 5

--- Wagon-0/05. What's your name ?/introduce_yourself.rb
+++ Wagon-0/05. What's your name ?/introduce_yourself.rb
@@ -1,1 +1,15 @@
 # your code goes here, introduce yourself !
+print "What's your first name?"
+first_name = gets.chomp
+first_name.capitalize!
+
+print "What's your middle name?"
+middle_name = gets.chomp
+middle_name.capitalize!
+
+print "What's your last name?"
+last_name = gets.chomp
+last_name.capitalize!
+
+puts "Hello, #{first_name} #{middle_name} #{last_name}"
+puts "Hello" + first_name + middle_name + last_name
