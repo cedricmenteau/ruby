ff50a13c3ecb3cdbdc4f644cc8a5296556d60791
Cédric M <me.cedric@gmail.com>
2014-01-07 09:47:06 +0100

    Third solution Ruby-0 challenge 6

--- Wagon-0/06. Stupid coaching/stupid_coaching.rb
+++ Wagon-0/06. Stupid coaching/stupid_coaching.rb
@@ -2,10 +2,12 @@ def stupid_coaching
   # your code goes here
   print "Anything to declare?"
   answer = gets.chomp
-
-  until answer.include? "I'm going to work"
-  	puts "Silly question, get dressed and go to work!" if answer == answer
-  else puts "I don't care son, get dressed and go to work !"
+  until answer == "I'm going to work"
+  	if answer.end_with?("?")
+      puts "Silly question, get dressed and go to work!" 
+    else puts "I don't care son, get dressed and go to work !"
+    puts "What else?"
+    answer = gets.chomp
   end
   puts "Good! Happy new year!"
 end
