7902d516671cf8843fe11c284d250c4ff71a4511
Olivier Lance <olance@users.noreply.github.com>
2014-01-13 10:22:15 +0100

    Code review 13/01
    Code en une seule ligne : pas besoin de convertir les Time en integer pour les soustraire, etc

--- Wagon-0/04. Age in days/age_in_days.rb
+++ Wagon-0/04. Age in days/age_in_days.rb
@@ -1,8 +1,11 @@
 def age_in_days(day, month, year)
   # your code goes here !
+  =begin
   t1 = Time.now.to_i
   t2 = Time.new(year, month, day).to_i
   t3 = (t1 - t2) / (3600.*24)
+  =end
+  ((Time.now - Time.new(year, month, day)) / (3600 * 24)).to_i
 end
 
 
