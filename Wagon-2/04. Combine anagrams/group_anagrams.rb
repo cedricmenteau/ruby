18c185a660a376fd0bc7407151973dc8b0e1d421
Cédric M <me.cedric@gmail.com>
2014-01-14 14:27:04 +0100

    A bunch of a lot

--- Wagon-2/04. Combine anagrams/group_anagrams.rb
+++ Wagon-2/04. Combine anagrams/group_anagrams.rb
@@ -1,5 +1,7 @@
 def group_anagrams(words)
-  # your code here
+  #sorting = Hash.new { |h, k| h[k] = 0 }
+  print word = words.collect { |x| x.split("").sort}
+  puts word.uniqu { |s| s.first}
 end
 
 # input:
@@ -10,3 +12,7 @@ group_anagrams( ['cars', 'for', 'potatoes', 'racs', 'four','scar', 'creams', 'sc
 
 # HINT: you can quickly tell if two words are anagrams by sorting their
 # letters, keeping in mind that upper vs lowercase doesn't matter
+
+#Transformer chacune des strings en sous-array
+#Spliter chacune des lettres des sous-arrays en string et les trier
+#Comparer chacune des sous-arrays et si identique += 1 
